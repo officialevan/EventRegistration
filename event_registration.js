83cbe55e6785e27a4b96be7d7faff241626425d6
officialevan <99198034+officialevan@users.noreply.github.com>
2023-06-23 13:45:11 +0300

    Update event_registration.js
    Remove dummy comments

--- event_registration.js
+++ event_registration.js
@@ -1,9 +1,3 @@
-/*
-	Your Name: <Enter Your Name>
-	Last Modified Date: <Enter The Date in mm/dd/yyyy format>
-	File: event_registration.js
-	File Description: <Enter a brief paragraph to describe the purpose of this file>
-*/
 
 // Set the minimum and maximum number of tickets able to be purchased
 var minTickets = 1;
